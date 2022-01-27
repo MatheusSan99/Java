9212de9c886f4132ff8b05d1a6620d235635f4db
MatheusSan99 <99matheussan@gmail.com>
2022-01-27 01:05:17 -0300

    Exercicios de treino bootcamp Dio

--- src/CarroObjetos.java
+++ src/CarroObjetos.java
@@ -1,12 +1,60 @@
-
+import java.util.Scanner;
 public class CarroObjetos {
+	Scanner sc = new Scanner(System.in);
+	
+		String Cor;
+		String Modelo;
+		int Capacidade;
+		double gasolina = (sc.nextDouble());
+		CarroObjetos() {
 
-	public static void main(String[] args) {
+		}
 		
-		String cor;
-		String modelo;
-		int capacidade;
-
-	}
+		CarroObjetos(String Cor, String Modelo, int Capacidade) {
+			this.Cor = Cor;
+			this.Modelo = Modelo;
+			this.Capacidade = Capacidade;		
+			
+		}	
+	
+		void setCor(String Cor) {
+			this.Cor = Cor;
+		}
+		
+		String getCor() {
+			return Cor;
+		}
+		
+		void setModelo(String Modelo) {
+			this.Modelo = Modelo;
+		}
+		
+		String getModelo() {
+			return Modelo;
+		}
+		
+		void Capacidade(int Capacidade) {
+			this.Capacidade = Capacidade;
+		}
+		
+		int Capacidade() {
+			return Capacidade;
+		}
+			
+			double valortanque() {
+			return Capacidade * gasolina;
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 
 }
---
+++ src/RodarCarro.java
@@ -0,0 +1,26 @@
+
+public class RodarCarro {
+
+	public static void main(String[] args)
+	
+	{
+		CarroObjetos carro1 = new CarroObjetos();
+		
+
+		carro1.setCor("Preto");
+		carro1.Capacidade(30);
+		carro1.setModelo("Gol quadrado");
+		
+		 
+		System.out.println(carro1.Capacidade());
+		System.out.println(carro1.getCor());
+		System.out.println(carro1.getModelo());
+		System.out.println(carro1.valortanque());
+		 
+		
+
+		 
+		 
+	}
+
+}
